c740ff86691a23ffc58876627384ae8275da982f
Vaibhav Shukla <vaibhavshuklamdi@gmail.com>
2025-04-23 00:49:32 +0530

    Updated Footer

--- Frontend/src/componentss/Reuse/Footer.jsx
+++ Frontend/src/componentss/Reuse/Footer.jsx
@@ -1,87 +1,84 @@
-import React from 'react'
+import React from 'react' 
 import logo from '../../images/MOOD.png'
-import { FaDiscord ,FaFacebookF, FaTwitter ,FaGithub,FaDribbble } from "react-icons/fa";
+import { FaDiscord, FaFacebookF, FaTwitter, FaGithub, FaDribbble } from "react-icons/fa";
 
 function Footer() {
   return (
-    
-
-<footer className="bg-white dark:bg-gray-900" id="footer">
-    <div className="mx-auto w-full max-w-screen-xl p-4 py-6 lg:py-8">
+    <footer className="bg-white dark:bg-gray-900" id="footer">
+      <div className="mx-auto w-full max-w-screen-xl p-4 py-6 lg:py-8">
         <div className="md:flex md:justify-between">
           <div className="mb-6 md:mb-0">
-              <a href="" className="flex items-center">
-                  <img src={logo} className="h-8 me-3" alt="MoodLogo" />
-                 
-              </a>
+            <a href="" className="flex items-center">
+              <img src={logo} className="h-8 me-3" alt="MoodLogo" />
+            </a>
           </div>
           <div className="grid grid-cols-2 gap-8 sm:gap-6 sm:grid-cols-3">
-              <div>
-                  <h2 className="mb-6 text-sm font-semibold text-gray-900 uppercase dark:text-white">Contact</h2>
-                  <ul className="text-gray-500 dark:text-gray-400 font-medium">
-                      <li className="mb-4">
-                          <a href="#" class="hover:underline">7232948707</a>
-                      </li>
-                      <li>
-                          <a href="#" class="hover:underline">rakeshpl25@gmail.com</a>
-                      </li>
-                  </ul>
-              </div>
-              <div>
-                  <h2 className="mb-6 text-sm font-semibold text-gray-900 uppercase dark:text-white">Follow us</h2>
-                  <ul className="text-gray-500 dark:text-gray-400 font-medium">
-                      <li className="mb-4">
-                          <a href="#" className="hover:underline ">Github</a>
-                      </li>
-                      <li>
-                          <a href="#" className="hover:underline">LinkedIn</a>
-                      </li>
-                  </ul>
-              </div>
-              <div>
-                  <h2 className="mb-6 text-sm font-semibold text-gray-900 uppercase dark:text-white">Legal</h2>
-                  <ul className="text-gray-500 dark:text-gray-400 font-medium">
-                      <li className="mb-4">
-                          <a href="#" class="hover:underline">Privacy Policy</a>
-                      </li>
-                      <li>
-                          <a href="#" clasName="hover:underline">Terms &amp; Conditions</a>
-                      </li>
-                  </ul>
-              </div>
+            <div>
+              <h2 className="mb-6 text-sm font-semibold text-gray-900 uppercase dark:text-white">Contact</h2>
+              <ul className="text-gray-500 dark:text-gray-400 font-medium">
+                <li className="mb-4">
+                  <a href="#" className="hover:underline">7232948707</a>
+                </li>
+                <li>
+                  <a href="#" className="hover:underline">rakeshpl25@gmail.com</a>
+                </li>
+              </ul>
+            </div>
+            <div>
+              <h2 className="mb-6 text-sm font-semibold text-gray-900 uppercase dark:text-white">Follow us</h2>
+              <ul className="text-gray-500 dark:text-gray-400 font-medium">
+                <li className="mb-4">
+                  <a href="#" className="hover:underline">Github</a>
+                </li>
+                <li>
+                  <a href="#" className="hover:underline">LinkedIn</a>
+                </li>
+              </ul>
+            </div>
+            <div>
+              <h2 className="mb-6 text-sm font-semibold text-gray-900 uppercase dark:text-white">Legal</h2>
+              <ul className="text-gray-500 dark:text-gray-400 font-medium">
+                <li className="mb-4">
+                  <a href="#" className="hover:underline">Privacy Policy</a>
+                </li>
+                <li>
+                  <a href="#" className="hover:underline">Terms &amp; Conditions</a>
+                </li>
+              </ul>
+            </div>
           </div>
-      </div>
-      <hr class="my-6 border-gray-200 sm:mx-auto dark:border-gray-700 lg:my-8" />
-      <div class="sm:flex sm:items-center sm:justify-between">
-          <span class="text-sm text-gray-500 sm:text-center dark:text-gray-400">© 2025 <a href="#" class="hover:underline">MOOD™</a>. All Rights Reserved.
+        </div>
+        <hr className="my-6 border-gray-200 sm:mx-auto dark:border-gray-700 lg:my-8" />
+        <div className="sm:flex sm:items-center sm:justify-between">
+          <span className="text-sm text-gray-500 sm:text-center dark:text-gray-400">
+            © 2025 <a href="#" className="hover:underline">MOOD™</a>. All Rights Reserved.
           </span>
-          <div class="flex mt-4 sm:justify-center sm:mt-0">
-              <a href="#" class="text-gray-500 hover:text-gray-900 dark:hover:text-white">
-                  <FaFacebookF />
-                  <span class="sr-only">Facebook page</span>
-              </a>
-              <a href="#" class="text-gray-500 hover:text-gray-900 dark:hover:text-white ms-5">
-                  <FaDiscord/>
-                  <span class="sr-only">Discord community</span>
-              </a>
-              <a href="#" class="text-gray-500 hover:text-gray-900 dark:hover:text-white ms-5">
-                  <FaTwitter />
-                  <span class="sr-only">Twitter page</span>
-              </a>
-              <a href="#" class="text-gray-500 hover:text-gray-900 dark:hover:text-white ms-5">
-                 <FaGithub />
-                  <span class="sr-only">GitHub account</span>
-              </a>
-              <a href="#" class="text-gray-500 hover:text-gray-900 dark:hover:text-white ms-5">
-                  <FaDribbble/>
-                  <span class="sr-only">Dribbble account</span>
-              </a>
+          <div className="flex mt-4 sm:justify-center sm:mt-0">
+            <a href="#" className="text-gray-500 hover:text-gray-900 dark:hover:text-white">
+              <FaFacebookF />
+              <span className="sr-only">Facebook page</span>
+            </a>
+            <a href="#" className="text-gray-500 hover:text-gray-900 dark:hover:text-white ms-5">
+              <FaDiscord />
+              <span className="sr-only">Discord community</span>
+            </a>
+            <a href="#" className="text-gray-500 hover:text-gray-900 dark:hover:text-white ms-5">
+              <FaTwitter />
+              <span className="sr-only">Twitter page</span>
+            </a>
+            <a href="#" className="text-gray-500 hover:text-gray-900 dark:hover:text-white ms-5">
+              <FaGithub />
+              <span className="sr-only">GitHub account</span>
+            </a>
+            <a href="#" className="text-gray-500 hover:text-gray-900 dark:hover:text-white ms-5">
+              <FaDribbble />
+              <span className="sr-only">Dribbble account</span>
+            </a>
           </div>
+        </div>
       </div>
-    </div>
-</footer>
-
+    </footer>
   )
 }
 
-export default Footer
+export default Footer;
